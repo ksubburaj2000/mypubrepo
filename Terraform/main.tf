219a995d521831332271834195b654c8f044eb66
ksubburaj2000 <109793801+ksubburaj2000@users.noreply.github.com>
2023-06-07 15:25:30 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,4 +1,4 @@
-/*
+
 resource "azurerm_resource_group" "target-resource-group" {
   name     = "rg-test001"
   location = "westeurope"
@@ -8,4 +8,3 @@ resource "azurerm_resource_group" "target-resource-group" {
   }
 }
 
-*/
